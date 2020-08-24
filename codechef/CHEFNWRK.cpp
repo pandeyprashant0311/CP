90b4b46ab6761ab92439e28569343914585a7f38
Abhi067852 <abhishek067852@gmail.com>
2020-08-24 08:37:32 +0530

    new codechef folder

---
+++ codechef/CHEFNWRK.cpp
@@ -0,0 +1,119 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<algorithm>
+#include<string.h>
+#include<stdio.h>
+using namespace std;
+#define ll long long int
+#define db double
+#define S second
+#define MOD 1000000007
+
+const int  N = 1e9 + 7;
+const int  P = 1e9;
+
+vector<ll> m;
+ll gcd (ll a, ll b) {return ( a ? gcd(b % a, a) : b );}
+
+ll power(ll a, ll n) {ll p = 1; while (n > 0) {if (n % 2) {p = p * a;} n >>= 1; a *= a;} return p;}
+ll power(ll a, ll n, ll mod) {ll p = 1; while (n > 0) {if (n % 2) {p = p * a; p %= mod;} n >>= 1; a *= a; a %= mod;} return p % mod;}
+ll get(ll x)
+{
+	ll c = 0;
+	while (x % 2 == 0)
+	{
+		c++;
+		x = x / 2;
+	}
+	for (ll i = 3; i < sqrt(x); i += 2)
+	{
+		while (x % i == 0)
+		{
+			c++;
+			x /= i;
+		}
+	}
+	if (x > 1)
+		c++;
+	return c;
+}
+ll maxnum(ll a[], ll x, ll y)
+{
+	map<ll, ll> m;
+	for (ll i = x; i <= y ; ++i)
+	{
+		m[a[i]]++;
+	}
+	ll h = INT_MIN;
+	ll s = INT_MAX;
+	for (auto i = m.begin(); i != m.end(); i++)
+	{
+		if (i->second > h)
+		{
+			h = i->second;
+			s = i->first;
+
+		}
+	}
+	cout << s << " " ;
+	return s;
+}
+void solve()
+{
+	int n, k;
+	cin >> n >> k;
+	int a[n];
+	for (int i = 0; i < n; ++i)
+	{
+
+
+		cin >> a[i];
+	}
+	int i = 0;
+	int ans = 0;
+
+	while (a[i] <= k)
+	{	int s = 0;
+		while (s + a[i] <= k)
+		{
+			i++;
+
+		}
+		ans++;
+
+
+	}
+	if (ans == 0)
+	{
+		cout << -1 << endl;
+	}
+	else
+		cout << ans << endl;
+
+}
+
+
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	ll i = 0;
+
+	while (i < t)
+	{
+
+		cout << "Case #" << i + 1 << ": ";
+		solve();
+		i++;
+	}
+	return 0;
+}
+
